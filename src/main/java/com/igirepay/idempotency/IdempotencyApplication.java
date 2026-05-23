400c96aa850ba1bb24e095a70443acf78d0c5610
Ishnaome <naomeimanishimwe7@gmail.com>
2026-05-23 18:32:25 +0200

    Application java

--- src/main/java/com/igirepay/idempotency/IdempotencyApplication.java
+++ src/main/java/com/igirepay/idempotency/IdempotencyApplication.java
@@ -7,8 +7,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class IdempotencyApplication {
+    
 
     public static void main(String[] args) {
         SpringApplication.run(IdempotencyApplication.class, args);
     }
 }
+// thi my application java file for the idempotency gateway. It is the entry point of the Spring Boot application. The @SpringBootApplication annotation indicates that this is a Spring Boot application, and the @EnableScheduling annotation allows us to use scheduled tasks in our application, which can be useful for cleaning up old idempotency records from the cache. 
